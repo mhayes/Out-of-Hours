ece5ac9c2afcbd16a3b896853185f569078a7bfb
Danny McClelland <mail@dannyweb.net>
2012-01-05 22:09:53 +0000

    Blank style for dashboard finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outofhours::Application.config.secret_token = '27b20c2cb5b17c5e477a12915b32d977e9b3d2c7c99bc1b980b6cf1209ab2939fa14723d3f9f47f316dbb3520d93470ef58c9b829cc9245641d1ffe0617adaf4'
